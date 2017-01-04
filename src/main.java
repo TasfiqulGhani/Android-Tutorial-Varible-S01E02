ac6e92f3db82ae3e7149753bda5443dc9c7c2f25
TasfiqulGhani <tasfiqulghani@gmail.com>
2017-01-04 22:54:12 +0600

    code

---
+++ src/main.java
@@ -0,0 +1,26 @@
+
+public class main {
+
+	 
+	public static void main(String[] args) {
+		 
+		double  age = 19.2;
+		int no=10;
+		String name="sadasd asdas asdas";
+		char c='a';
+		
+		
+		
+		
+		// datatype int
+		// varible age
+		// value 19
+		
+		
+		
+		
+		System.out.print("Tashfik");
+
+	}
+
+}
